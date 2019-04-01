57977dbcb852763ec2b7550b4824e6b65aede480
Adelys Belen <adalbeca@gmail.com>
2019-04-01 10:15:40 -0300

    Fix layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch, IndexRoute } from 'react-router-dom';
-import FormDataRegister  from  './screen/FormData';
-import GridData from  './screen/GridData';
 import Landpage from './screen/Landpage';
 import './App.css';
 import 'antd/dist/antd.css';
 
-
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
-          <Switch>
-              <Route  path='/' component={Landpage} />
-              <Route  path='/add' component={FormDataRegister} />
-
-          </Switch>
-        </BrowserRouter>
+        <Landpage/>
     );
   }
 }
--- src/screen/Landpage/index.js
+++ src/screen/Landpage/index.js
@@ -1,37 +1,42 @@
-import React  from 'react';
-import {
-   Row, Col, Layout } from 'antd';
+import React from 'react';
+import {Row, Col, Layout} from 'antd';
 
-import Logo from  '../../assets/logo-despegar.png';
+import Logo from '../../assets/logo-despegar.png';
 import FormDataRegister from '../FormData';
 import GridData from '../GridData'
-import {Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 
-export default class Landpage extends React.Component{
+export default class Landpage extends React.Component {
 
-    componentDidMount(){
+    componentDidMount() {
         document.title = "Delivery";
     }
 
-    render(){
-        const { Header, Content, Footer } = Layout;
-        return(
-            <Layout className="layout" >
-                <Row className={'mb-2 border header-des'} >
-                    <Col lg={24} className="d-flex flex-row align-items-center justify-content-between" style={{padding:10}}>
+    render() {
+        const {Header, Content, Footer} = Layout;
+        return (
+            <Layout className="layout">
+                <Row className={'mb-2 border header-des'}>
+                    <Col lg={24} className="d-flex flex-row align-items-center justify-content-between"
+                         style={{padding: 10}}>
                         <span>Delivery Online</span>
                         <img src={Logo} alt={'logo'} style={{height:40}}/>
                     </Col>
                 </Row>
                 <Content className='content'>
-                    <GridData/>
-                        <Route path='/add' component={FormDataRegister}/>
-                        <Route path='/grid' component={GridData}/>
+                    <BrowserRouter>
+                        <Switch>
+                            <Route exact path='/' component={GridData}/>
+                            <Route exact path='/add' component={FormDataRegister}/>
+                        </Switch>
+                    </BrowserRouter>
                 </Content>
-                <Footer  style={{ textAlign: 'center', backgroundColor:'#638abb', color:'#fff', height:40, marginTop:20 }}>
-                    Test despegar.com
+                <Footer
+                    style={{textAlign: 'center', backgroundColor: '#638abb', color: '#fff', height: 40, marginTop: 20}}>
+                    Test
                 </Footer>
             </Layout>
-        )}
+        )
+    }
 }
